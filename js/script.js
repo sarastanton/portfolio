efeb1e3b36567e49b44654aa88a5a3bd85baadf2
Sara Stanton <sara.mpa13@gmail.com>
2020-02-16 03:38:00 -0600

    Cleanup

--- js/script.js
+++ js/script.js
@@ -1,14 +1,4 @@
-//
-// $(document).ready(function() {
-//
-//
-// });
-
-
 $(window).on("load", function() {
-  // $(".loader .inner").fadeOut(500, function() {
-  //   $(".loader").fadeOut(750);
-  // });
 
   $("items").isotope({
     filter: '*',
@@ -22,13 +12,8 @@ $(window).on("load", function() {
 
 $(document).ready(function() {
 
-  // var height = $(window).height();
-  //
-  //   $('#header').css('height', height);
-
   //Scrollspy plugin
   $('body').scrollspy({ target: '.navbar-spy'});
-
 
   // Superslides
   $('#slides').superslides({
@@ -37,16 +22,6 @@ $(document).ready(function() {
     inherit_height_from: '#slides-wrapper',
     inherit_width_from: '#slides-wrapper'
   });
-
-  // Typed JS
-  // const typed = new Typed(".typed", {
-  //   strings: ["Full Stack Web Developer"],
-  //   // can add other text to the array above
-  //   typeSpeed: 70,
-  //   loop: true,
-  //   startDelay: 1000,
-  //   showCursor: false,
-  // });
 
   // Owl Carousel
   $('.owl-carousel').owlCarousel({
@@ -68,49 +43,7 @@ $(document).ready(function() {
     }
   });
 
-  // const skillsTopOffset = $(".skillsSection").offset().top;
-  // const statsTopOffset = $(".statsSection").offset().top;
-  // let countUpFinished = false;
-
-  // $(window).scroll(function() {
-  //
-  //   if(window.pageYOffset > skillsTopOffset - $(window).height() + 200) {
-  //     // Easy Pie Chart
-  //     $('.chart').easyPieChart({
-  //         easing: 'easeInOut',
-  //         barColor: 'white',
-  //         trackColor: false,
-  //         scaleColor: false,
-  //         lineWidth: 4,
-  //         size: 152,
-  //         onStep: function(from, to, percent) {
-  //           $(this.el).find('.percent').text(Math.round(percent));
-  //         }
-  //     });
-  //   };
-  //
-  //   if(!countUpFinished && window.pageYOffset > statsTopOffset - $(window).height() + 200) {
-  //     // countUp
-  //     $(".counter").each(function() {
-  //       const element = $(this);
-  //       const endVal = parseInt(element.text());
-  //
-  //       element.countup(endVal);
-  //     });
-  //
-  //     countUpFinished = true;
-  //   };
-  //
-  // });
-
-  $("[data-fancybox]").fancybox({
-        // helpers: {
-        //     media: {}
-        // },
-        // iframe : {
-		    //   preload : false
-	      // }
-    });
+  $("[data-fancybox]").fancybox();
 
   $("#filters a").click(function() {
 
@@ -136,7 +69,6 @@ $(document).ready(function() {
 
     const targetElement = $(this).attr("href");
     const blogLink = $('a:contains("Blog")').attr("href");
-    // debugger
     if(targetElement != blogLink) {
       e.preventDefault();
       const targetPosition = $(targetElement).offset().top;
@@ -179,7 +111,7 @@ $(document).ready(function() {
   };
 
   $('footer').text(`${copy()} Sara Stanton ${(new Date()).getFullYear()}`)
-  
+
 
   // $(window).scroll(function() {
   //     var position = $(this).scrollTop();
